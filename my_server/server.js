c33cc7edebe7082bda7c7750243c0b96aa40321e
chapaedb <chapaeresso1@gmail.com>
2024-08-13 14:40:00 +0300

    Added a config direcory with database configuration

--- my_server/server.js
+++ my_server/server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const router = require('./routes')
+const dotenv = require('dotenv')
+const connectMongodb = require('./config/db')
 
+dotenv.config()
+
+console.log('MongoDB Connection URL:', process.env.CONNECTION_URL);
+
+connectMongodb();
+
+app.use(express.json());
+app.use('/', router)
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
 
-app.use('/', router)
+
