6e48d1502d231775bb92c117c6d5bb99db226055
aliola123 <alipoemoses6461@gmail.com>
2024-08-16 07:59:13 +0100

    wrote a new code for login

--- my_server/server.js
+++ my_server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const router = require('./routes')
+const router = require('./routes/routes')
 const dotenv = require('dotenv')
 const connectMongodb = require('./config/db')
+
 
 dotenv.config()
 
