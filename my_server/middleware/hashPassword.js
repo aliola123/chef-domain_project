59ca695316083c676fbd54a6912b73b6932b3f8b
aliola123 <alipoemoses6461@gmail.com>
2024-08-20 08:11:22 +0100

    Changed the module bcrypt to bcryptjs as bcrypt is deprecated and requires a beter module to avoid security breaches

--- my_server/middleware/hashPassword.js
+++ my_server/middleware/hashPassword.js
@@ -1,10 +1,10 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const hashPassword = async (req, res, next) => {
     try {
         if (req.body.password) {
-            const salt = await bcrypt.genSalt(10);
-            req.body.password = await bcrypt.hash(req.body.password, salt);
+            const salt = await bcryptjs.genSalt(10);
+            req.body.password = await bcryptjs.hash(req.body.password, salt);
         }
         next();
     } catch (error) {
--- my_server/utils/comparePassword.js
+++ my_server/utils/comparePassword.js
@@ -1,7 +1,7 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const comparePassword = async (password, hashed)=>{
-    return await bcrypt.compare(password, hashed);
+    return await bcryptjs.compare(password, hashed);
 }
 
 module.exports = comparePassword;
